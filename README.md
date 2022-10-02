adcb6686023d3a1bfe500a271799b6ca1caf7eb0
Davide <davide@codref.org>
2022-10-02 13:07:24 +0200

    Removed graylog

--- README.md
+++ README.md
@@ -10,15 +10,15 @@ Languages available as templates:
 - python3.10-microservice
 ```
 
-## python3-microservice
+## python3.x-microservice
 
-The template _python3-microservice_ is equivalent to [`python3`](https://github.com/openfaas/python-flask-template) with the exception being this template uses **Graylog** for Waitress logging and supports a *microservice* architecture, rather than a pure Function As A Service one.
+The template _python3-microservice_ is equivalent to [`python3`](https://github.com/openfaas/python-flask-template) with the exception that it supports a *microservice* architecture, rather than a pure Function As A Service one (more freedom in defining the functions).
 
 
 ### Using the templates
 
 ```
-$ faas-cli template pull https://gitlab.evouser.com/infrastructure/faas-templates
+$ faas-cli template pull https://github.com/codref/faas-templates.git
 $ faas-cli new --lang <template> <fn-name>
 ```
 
--- template/python3.10-microservice/index.py
+++ template/python3.10-microservice/index.py
@@ -2,43 +2,6 @@
 from flask import Flask
 from flask_cors import CORS
 from waitress import serve
-from paste.translogger import TransLogger 
-import graypy, logging
-import os
-
-APP_NAME=os.getenv('APP_NAME', 'faas-python-app')
-ENV_NAME=os.getenv('ENV_NAME', 'local')
-LOG_LEVEL=os.getenv('LOG_LEVEL', 'INFO')
-
-logging.basicConfig(level=logging.getLevelName(LOG_LEVEL),
-                    format=f'%(asctime)s %(message)s',
-                    handlers=[logging.StreamHandler()]
-)
-
-# Decorate logs with some useful additional metadata
-class GelfFilter(logging.Filter):
-    def filter(self, record):
-        record.env_name = ENV_NAME
-        record.app_name = APP_NAME
-        return True
-
-# Initialize Graylog handler for logging
-GRAYLOG_HOST=os.getenv('GRAYLOG_HOST')
-GRAYLOG_PORT=os.getenv('GRAYLOG_PORT')
-GRAYLOG_PROTOCOL=os.getenv('GRAYLOG_PROTOCOL', 'tcp')
-
-if not GRAYLOG_HOST is None and not GRAYLOG_PORT is None:
-    gelf_handler = None
-    if GRAYLOG_PROTOCOL == 'tcp':
-        gelf_handler = graypy.GELFTCPHandler(GRAYLOG_HOST, GRAYLOG_PORT)
-    elif GRAYLOG_PROTOCOL == 'udp':
-        gelf_handler = graypy.GELFUDPHandler(GRAYLOG_HOST, GRAYLOG_PORT)
-
-    if not gelf_handler is None:
-        gelf_handler.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
-        gelf_handler.addFilter(GelfFilter())
-        logging.getLogger().addHandler(gelf_handler)
-
 
 # we instantiate Flask first, to support future blueprinting
 app = Flask(__name__)
@@ -49,4 +12,4 @@ from function import handler
 
 # Start wsgi application through Waitress server
 if __name__ == '__main__':
-    serve(TransLogger(app, setup_console_handler=False), host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
